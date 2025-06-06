35c2fb0996c97557960acc091112d79c9d45fbb7
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-06 18:40:45 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,7 @@
 import { useEffect, useState } from 'react';
-import { FeedbackOptions } from './FeedbackOptions';
 import { Section } from './Section';
-import { Statistics } from './Statistics';
-import { Notification } from './Notification';
+
 
 export const App = () => {
-  const [feedback, setFeedback] = useState(() => {
-    const saved = sessionStorage.getItem('feedback');
-    return saved ? JSON.parse(saved) : { good: 0, neutral: 0, bad: 0 };
-  });
-  useEffect(() => {
-    sessionStorage.setItem('feedback', JSON.stringify(feedback));
-  }, [feedback]);
-
-  const handleLeaveFeedback = type => {
-    setFeedback(prev => ({
-      ...prev,
-      [type]: prev[type] + 1,
-    }));
-  };
-
-  const total = feedback.good + feedback.neutral + feedback.bad;
-  const positivePercentage =
-    total > 0 ? Math.round((feedback.good / total) * 100) : 0;
-  return (
-    <div
-      style={{
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 30,
-        color: '#010101',
-      }}
-    >
-      <Section title="Leave your Feedback">
-        <FeedbackOptions
-          options={Object.keys(feedback)}
-          onLeaveFeedback={handleLeaveFeedback}
-        />
-      </Section>
-      <Section title="Statistics">
-        {total > 0 ? (
-          <Statistics
-            good={feedback.good}
-            neutral={feedback.neutral}
-            bad={feedback.bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </Section>
-    </div>
-  );
+  
 };
